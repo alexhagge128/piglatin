e4d84269a8189a937ded2b91487f87f7988ec6b6
Amanda Daly and Alex Hagge <alexhagge128@gmail.com>
2016-02-22 19:57:17 -0800

    Added restrictions on number field

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
-  } else {
+  }
+  else {
     return false;
   }
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (year < 0) {
+      alert("No negative numbers please");
+    }
+    else if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+      $("#result").show();
+    }
+    else {
+      $(".not").text("");
+      $("#result").show();
+    }
+
+    event.preventDefault();
+  });
+});
